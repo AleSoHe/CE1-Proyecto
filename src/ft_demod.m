6c01f0f08d83af3e2a0c4bd953e29c63523e5d2b
alesz19 <49692402+alesz19@users.noreply.github.com>
2019-05-14 21:34:38 -0600

    Update ft_demod.m

--- src/ft_demod.m
+++ src/ft_demod.m
@@ -1,12 +1,28 @@
-Demodulacion para Natural y Flat-Top
- function[t1,f,original_t_signal,original_f_signal]=Demod(st,fm) %funcion de demodulacion, recibe la senal modulada, fm y fs y tiene de salidas el tiempo y la funcion de la onda original, asi como frecuencia y la onda transformada en f.(Propositos de ploteo)
- fs= 100*fm
- st_frequency=fftshift(abs(fft(st))); %Transformada Rapida de Fourier(FFT) de s(t)
-filter=fir1(200,fm/fs,'low'); %funcion del LPF 
-original_t_signal=conv(filter,st); %obtencion de funcion en t original
-original_f_signal=fftshift(abs(fft(original_t_signal))); %obtencion de funcion en f original usando FFT
-t1=0:1/(length(original_t_signal)-1):1; %periodo de tiempo para plot
-f=-fs/2:fs/(length(original_f_signal)-1):fs/2; %frecuencia para plot
+%Demodulacion para Natural y Flat-Top
+
+%Funcion de demodulacion, recibe la senal modulada, fm y fs y tiene de salidas el tiempo y la funcion de la onda original, asi como frecuencia y la onda transformada en f.(Propositos de ploteo)
+ function[t1,f,original_t_signal,original_f_signal]=Demod(st,fm) 
+ 
+ fs = 10*fm
+%Calculo de la transformada Rapida de Fourier para propositos de ploteo
+ st_frequency=fftshift(abs(fft(st))); 
+ 
+ %Filtro de Demodulacion LPF
+filter=fir1(200,fm/fs,'low');
+
+ %reconstruccion de funcion en t original
+original_t_signal=conv(filter,st);
+
+%obtencion de funcion en f original usando FFT
+original_f_signal=fftshift(abs(fft(original_t_signal))); 
+
+%periodo de tiempo para plot
+t1=0:1/(length(original_t_signal)-1):1; 
+
+%frecuencia para plot
+f=-fs/2:fs/(length(original_f_signal)-1):fs/2; 
+
+%ploteo
 figure(2)
  subplot(2,1,1);
  plot(t1,original_t_signal);
@@ -18,4 +34,5 @@ figure(2)
  title('Dominio de la frecuencia');
  xlabel('Frecuencia');
  ylabel('Amplitud');
+© 2019 GitHub, Inc.
  
