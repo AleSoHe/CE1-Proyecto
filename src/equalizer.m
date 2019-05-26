2fb759acff6b05671115f9d0abaea5921fc8ae23
AleSoHe <30560227+AleSoHe@users.noreply.github.com>
2019-05-25 22:38:21 -0600

    Update equalizer.m

--- src/equalizer.m
+++ src/equalizer.m
@@ -1,15 +1,14 @@
 function output = equalizer(input, Fs, BW)
 
 % This function receives an "input" signal, of bandwidth "BW", sampled with
-% a "Fs" frequency.
+% a "Fs" frequency
 % The input signal is processed by an equalizer (1/sa), and the result is
-% the "output" of the function
+% the output of the function
 
-% Load piano.mp3, for testing purposes
-[input,Fs] = audioread('..\audio\piano.mp3');
+% [input,Fs] = audioread('..\audio\piano.mp3');
 
 % Signal resampling
-n = 20;
+n = 10;
 input = resample(input,1,n);
 Fs = Fs/n;
 
@@ -18,13 +17,13 @@ norm_cutoff = BW/Fs;
 
 % Filter design
 d = fdesign.isinclp('N,Fp,Fst',50,norm_cutoff,norm_cutoff+0.1);
-Hd = design(d,'SincFrequencyFactor',0.5,'SincPower',4,'SystemObject',true);
+Hd = design(d,'SincFrequencyFactor',0.9,'SincPower',5,'SystemObject',true);
 fvtool(Hd, 'MagnitudeDisplay', 'Magnitude');
 
 % Signal equalization
 output = Hd(input);
 
 % Play music
-sound(output,Fs);
+%sound(output,Fs);
 
 
