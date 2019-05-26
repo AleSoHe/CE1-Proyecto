58adb2f705ff34a3e6ef0752bffe0da3fa0768c2
AleSoHe <30560227+AleSoHe@users.noreply.github.com>
2019-05-26 00:14:46 -0600

    Update equalizer.m

--- src/equalizer.m
+++ src/equalizer.m
@@ -1,29 +1,23 @@
-function output = equalizer(input, Fs, BW)
+function error = metrics(original,demod)
+    % metrics for a modulated/demodulated signal
+    % INPUTS:
+    %   original: signal without modulation and free of noise
+    %   demod: signal after modulation, noise, and demodulation
+    % OUTPUTS:
+    %   
 
-% This function receives an "input" signal, of bandwidth "BW", sampled with
-% a "Fs" frequency
-% The input signal is processed by an equalizer (1/sa), and the result is
-% the output of the function
+    % Fix phase shift of demodulated signal due to filters, channel, etc.
 
-% [input,Fs] = audioread('..\audio\piano.mp3');
+    % Original signal padding, as lengths could be different
 
-% Signal resampling
-n = 10;
-input = resample(input,1,n);
-Fs = Fs/n;
+    % Compute signals power
+    original_power = (original.^2)./length(original);
+    demod_power = (demod.^2)./length(demod);
 
-% Compute normialized cutoff frequency
-norm_cutoff = BW/Fs;
+    % Demodulated signal's power normalization
+    demod = demod.*(original_power./demod_power);
 
-% Filter design
-d = fdesign.isinclp('N,Fp,Fst',50,norm_cutoff,norm_cutoff+0.1);
-Hd = design(d,'SincFrequencyFactor',0.9,'SincPower',5,'SystemObject',true);
-fvtool(Hd, 'MagnitudeDisplay', 'Magnitude');
-
-% Signal equalization
-output = Hd(input);
-
-% Play music
-%sound(output,Fs);
-
+    % Error cuadrático medio
+    error = ((demod-origial).^2)./length(demod);
+end
 
