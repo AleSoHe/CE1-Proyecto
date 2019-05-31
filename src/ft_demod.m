8614ecd2e98fa5017e81496cc03559cf94be2aae
alesz19 <49692402+alesz19@users.noreply.github.com>
2019-05-31 15:52:35 -0600

    Update ft_demod.m

--- src/ft_demod.m
+++ src/ft_demod.m
@@ -1,16 +1,19 @@
 %Demodulacion para Natural y Flat-Top
  
-function[t1,f,original_t_signal,original_f_signal]=Demod(st,Fc,Fs) %funcion de demodulacion 
+function[t1,f,original_t_signal,original_f_signal]=Demod(st,Fc,Fs) %Demodulation Function
+%inputs: signal, single tone frecuency (cutoff frecuency), sample frecuency.
 
-    filter = butter(15,Fc/(Fs/2),'low'); %funcion del LPF con fc normalizada
+%outputs: reconstructed time vector, original signal, original signal spectrum.
+
+    filter = butter(15,Fc/(Fs/2),'low'); % butterworth 15th order LPF with cutoff frecuency Fc/(Fs/2)
     
-    original_t_signal = conv(filter,st); %convolucion de la funcion de transferencia del filtro LPF con la senal modulada
+    original_t_signal = conv(filter,st); %convolution of filter function with st in the time domain
     
-    original_f_signal = fftshift(abs(fft(original_t_signal))); %obtencion de funcion en f original usando FFT
+    original_f_signal = fftshift(abs(fft(original_t_signal))); %Absolute Value of Fast Fourier Transform  of the demodulated signal
     
-    t1 = 0:1/Fs:(1/Fs)*length(original_t_signal)-1/Fs; %periodo de tiempo para plot
+    t1 = 0:1/Fs:(1/Fs)*length(original_t_signal)-1/Fs; %time span vector
     
-    f =-Fc*100/2:Fc*100/(length(original_f_signal)-1):Fc*100/2; %frecuencia para plot
+    f =-Fc*100/2:Fc*100/(length(original_f_signal)-1):Fc*100/2; %frecuency span vector
     
     %plot
     
