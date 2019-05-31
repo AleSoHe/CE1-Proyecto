35999fa8a5f497f0fe8b73db1e8af3e6e1f73fa1
alesz19 <49692402+alesz19@users.noreply.github.com>
2019-05-31 15:47:01 -0600

    Update ft_demod.m

--- src/ft_demod.m
+++ src/ft_demod.m
@@ -1,38 +1,32 @@
 %Demodulacion para Natural y Flat-Top
-
-%Funcion de demodulacion, recibe la senal modulada, fm y fs y tiene de salidas el tiempo y la funcion de la onda original, asi como frecuencia y la onda transformada en f.(Propositos de ploteo)
- function[t1,f,original_t_signal,original_f_signal]=Demod(st,fm) 
  
- fs = 10*fm
-%Calculo de la transformada Rapida de Fourier para propositos de ploteo
- st_frequency=fftshift(abs(fft(st))); 
+function[t1,f,original_t_signal,original_f_signal]=Demod(st,Fc,Fs) %funcion de demodulacion 
+
+    filter = butter(15,Fc/(Fs/2),'low'); %funcion del LPF con fc normalizada
+    
+    original_t_signal = conv(filter,st); %convolucion de la funcion de transferencia del filtro LPF con la senal modulada
+    
+    original_f_signal = fftshift(abs(fft(original_t_signal))); %obtencion de funcion en f original usando FFT
+    
+    t1 = 0:1/Fs:(1/Fs)*length(original_t_signal)-1/Fs; %periodo de tiempo para plot
+    
+    f =-Fc*100/2:Fc*100/(length(original_f_signal)-1):Fc*100/2; %frecuencia para plot
+    
+    %plot
+    
+    figure(2)
+     subplot(2,1,1);
+     plot(t1,original_t_signal);
+     title('Dominio del tiempo de la senal Demodulada');
+     xlabel('Tiempo');
+     ylabel('Amplitud');
+     
+    subplot(2,1,2);
+     plot(f,original_f_signal);
+     title('Dominio de la frecuencia');
+     xlabel('Frecuencia');
+     ylabel('Amplitud');
+
  
- %Filtro de Demodulacion LPF
-filter=fir1(200,fm/fs,'low');
-
- %reconstruccion de funcion en t original
-original_t_signal=conv(filter,st);
-
-%obtencion de funcion en f original usando FFT
-original_f_signal=fftshift(abs(fft(original_t_signal))); 
-
-%periodo de tiempo para plot
-t1=0:1/(length(original_t_signal)-1):1; 
-
-%frecuencia para plot
-f=-fs/2:fs/(length(original_f_signal)-1):fs/2; 
-
-%ploteo
-figure(2)
- subplot(2,1,1);
- plot(t1,original_t_signal);
- title('Dominio del tiempo de la senal Demodulada');
- xlabel('Tiempo');
- ylabel('Amplitud');
- subplot(2,1,2);
- plot(f,original_f_signal);
- title('Dominio de la frecuencia');
- xlabel('Frecuencia');
- ylabel('Amplitud');
 © 2019 GitHub, Inc.
  
