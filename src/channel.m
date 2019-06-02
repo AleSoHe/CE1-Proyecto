8eee1e676dfeb42295d61edc7824166d30c0dca0
maxhr08 <maxhr08@gmail.com>
2019-06-02 16:21:21 -0600

    missing semicolon line 4

--- src/channel.m
+++ src/channel.m
@@ -1,7 +1,7 @@
 function out_signal = channel(time,in_signal,snr,atn)
    
     out_signal = awgn(in_signal,snr); %suma ruido gaussinano blanco
-    out_signal = atn * out_signal  %atenua por el factor de perdidas del canal
+    out_signal = atn * out_signal;  %atenua por el factor de perdidas del canal
     figure(1)
     plot(time,out_signal)
     pause(5)
