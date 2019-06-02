8a19438b7d595fd7c2a136c796c6c3969f60caf4
alesz19 <49692402+alesz19@users.noreply.github.com>
2019-06-01 22:04:17 -0600

    Update demod.m

--- src/demod.m
+++ src/demod.m
@@ -5,9 +5,13 @@ function[t1,f,original_t_signal,original_f_signal]=Demod(st,Fc,Fs) %Demodulation
 
 %outputs: reconstructed time vector, original signal, original signal spectrum.
 
-    filter = butter(15,Fc/(Fs/2),'low'); % butterworth 15th order LPF with cutoff frecuency Fc/(Fs/2)
+    filter = butter(12,Fc/(Fs/2),'low'); % butterworth 12th order LPF with cutoff frecuency Fc/(Fs/2)
     
-    original_t_signal = conv(filter,st); %convolution of filter function with st in the time domain
+    gain_compdB = 12*20 + 20 + 3; % compensation factor for LPF order amplitude loss plus compensation of cutoff frecuency amp loss (in dB)
+    
+    gain_comp = 10^(gain_compdB/20) % compensation factor for LPF amplitude loss
+   
+    original_t_signal = gain_comp*conv(filter,st); %convolution of filter function with st in the time domain
     
     original_f_signal = fftshift(abs(fft(original_t_signal))); %Absolute Value of Fast Fourier Transform  of the demodulated signal
     
