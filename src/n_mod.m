e9e6a74b2267f554f619026e1a69c3f534c9bbba
maxhr08 <maxhr08@gmail.com>
2019-06-02 23:12:12 -0600

    final commit of June/2/2019

--- src/n_mod.m
+++ src/n_mod.m
@@ -1,21 +1,21 @@
-function [R] = n_mod(y,Fs,D,A)
-    len = length(y(:, 1));
-    R = zeros(len*A, 2);
+function [mod_sig] = n_mod(message,Fs,D,A)
+    len = length(message(:, 1));
+    mod_sig = zeros(len*A, 2);
     EA = D*A;
     for i = A:A*len+A-1
         %modulate
         if(mod(floor(i/EA), 2) == 0)
             %Oversample by A
-            R(i-A+1, :) = y(floor(i/A), :);
+            mod_sig(i-A+1, :) = message(floor(i/A), :);
         end
     end
-    %{
+    
     in = 1:300;
     in2 = 1:300*A;
     in = in/Fs;
     in2 = [in2 + (A-1)]/Fs/A;
-    plot(in, y(1:300, 1))
+    plot(in, message(1:300, 1))
     hold on
-    plot(in2, R(1:300*A, 1))
-    %}
+    plot(in2, mod_sig(1:300*A, 1))
+    
 end
