e3a341aaf5f61c9daae7691b3f52ee8122c2242c
Alejandro <alejandrosohe@hotmail.com>
2019-06-02 19:12:23 -0600

    Update channel.m

--- src/channel.m
+++ src/channel.m
@@ -1,8 +1,10 @@
-function out_signal = channel(time,in_signal)
+function [out_signal,noise] = channel(time,in_signal)
     snr=15;
     out_signal = awgn(in_signal,snr);
     figure(1)
-    hold on
     plot(time,out_signal)
+    waitforbuttonpress;
+    noise = out_signal - in_signal;
+    plot(time,noise)
     waitforbuttonpress;
 end
