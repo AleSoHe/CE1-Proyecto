090fe7d010704ed94bdefaaa4f3f02fda238418d
MiBoza <49692557+MiBoza@users.noreply.github.com>
2019-06-03 18:54:57 -0600

    Update n_mod.m

--- src/n_mod.m
+++ src/n_mod.m
@@ -2,11 +2,16 @@ function [mod_sig] = n_mod(message,Fs,D,A)
     len = length(message(:, 1));
     mod_sig = zeros(len*A, 2);
     EA = D*A;
+    message(len+1, :) = [0, 0];
     for i = A:A*len+A-1
         %modulate
         if(mod(floor(i/EA), 2) == 0)
             %Oversample by A
-            mod_sig(i-A+1, :) = message(floor(i/A), :);
+            a = floor(i/A);
+            b = ceil(i/A);
+            c = message(a, :);
+            d = message(b, :);
+            mod_sig(i-A+1, :) = (d-c)*(i/A-a) + c;
         end
     end
     
@@ -14,8 +19,8 @@ function [mod_sig] = n_mod(message,Fs,D,A)
     in2 = 1:300*A;
     in = in/Fs;
     in2 = [in2 + (A-1)]/Fs/A;
-    plot(in, message(1:300, 1))
+    figure(1), plot(in, message(1:300, 1))
     hold on
-    plot(in2, mod_sig(1:300*A, 1))
+    figure(1), plot(in2, mod_sig(1:300*A, 1))
     
 end
