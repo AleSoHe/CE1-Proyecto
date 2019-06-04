bd3dca6ad2c3daed4c243dc134d594bcbeb4f214
alesz19 <49692402+alesz19@users.noreply.github.com>
2019-06-04 11:12:18 -0600

    Update channel.m

--- src/channel.m
+++ src/channel.m
@@ -3,5 +3,8 @@ function out_signal = channel(time,in_signal,snr,atn)
     out_signal = atn * out_signal;  %atenua por el factor de perdidas del canal
     figure(1)
     plot(time,out_signal)
+    title("Ruido blanco")
+    xlabel("Tiempo(s)")
+    ylabel("Amplitud")
     pause(5)
 end
