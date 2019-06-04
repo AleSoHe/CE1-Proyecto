e4176413f5140c155b7b6775ff88da0eceda93e0
Alejandro <alejandrosohe@hotmail.com>
2019-06-04 02:37:24 -0600

    Update metrics.m

--- src/metrics.m
+++ src/metrics.m
@@ -1,23 +1,37 @@
-function error = metrics(original,demod)
+function [MSE,SNR] = metrics(original,demod_sig,demod_noise)
     % metrics for a modulated/demodulated signal
     % INPUTS:
     %   original: signal without modulation and free of noise
     %   demod: signal after modulation, noise, and demodulation
     % OUTPUTS:
-    %   
+    %   MSE: mean squared error
+    %   SNR: signal to noise ratio
 
     % Fix phase shift of demodulated signal due to filters, channel, etc.
 
     % Original signal padding, as lengths could be different
 
     % Compute signals power
-    original_power = (original.^2)./length(original);
-    demod_power = (demod.^2)./length(demod);
+    original_power = sum((original.^2))./length(original);
+    demod_power = sum((demod_sig.^2))./length(demod_sig);
 
     % Demodulated signal's power normalization
-    demod = demod.*(original_power./demod_power);
+    demod_norm = demod_sig.*(original_power/demod_power);
 
-    % Error cuadrÃ¡tico medio
-    error = ((demod-origial).^2)./length(demod);
+    % Mean Squared Error
+    MSE = sum(((demod_norm-original').^2))/length(demod_norm);
+    
+    % Noise
+    noise = demod_noise - demod_sig; % demodulated signal - demodulated signal with noise
+    
+    % Noise power
+    noise_power = sum(noise.^2)/length(noise);
+    
+    % Signal to noise ratio (output)
+    SNR = demod_power/noise_power;
+    
+    disp(['Error cuadrático medio: ',num2str(MSE)]);
+    disp(['Razón de señal a ruido: ',num2str(SNR)]);
+    
 end
 
